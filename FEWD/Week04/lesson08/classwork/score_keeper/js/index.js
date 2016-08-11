a466d6daaaa68c507514dd4da95a3d43595f2f3b
jacqwalker <blackjack.walker@gmail.com>
2016-08-11 20:52:12 +0100

    Matchmaker solution

---
+++ FEWD/Week04/lesson08/classwork/score_keeper/js/index.js
@@ -0,0 +1,27 @@
+var total = 0;
+
+document.querySelector("#zero").onclick = zero;
+document.querySelector("#add5").onclick = add5;
+document.querySelector("#add10").onclick = add10;
+document.querySelector("#sub1").onclick = sub1;
+
+function zero () {
+  total = 0;
+  document.querySelector("#result").innerHTML = total;
+}
+
+function add5 () {
+  total = total + 5;
+  //you need to reassign total now to the new total//
+  document.querySelector("#result").innerHTML = total;
+}
+
+function add10 () {
+  total = total + 10;
+  document.querySelector("#result").innerHTML = total;
+}
+
+function sub1 () {
+  total = total - 1;
+  document.querySelector("#result").innerHTML = total;
+}
