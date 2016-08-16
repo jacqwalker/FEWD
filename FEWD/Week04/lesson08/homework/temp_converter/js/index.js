688181c01c80362e53bbaafb8eb263f41a3c6388
jacqwalker <blackjack.walker@gmail.com>
2016-08-16 19:37:27 +0100

    Review of temp converter - 1 & 2

--- FEWD/Week04/lesson08/homework/temp_converter/js/index.js
+++ FEWD/Week04/lesson08/homework/temp_converter/js/index.js
@@ -1,10 +1,57 @@
 //Do stuff
 
-function conversion () {
-  var c = document.querySelector(".input").value;
-  var f = c*9/5+32;
+//Get the value of the input - not at the point of loading the page, only when we get the input therefore it goes in a function
+// get the button
 
-  document.querySelector("span").innerHTML = f;
+// function convert () {
+//   console.log("hello world");
+// TO CHECK THE BUTTON IS WORKING}
+
+// STEP 1 = get the data
+// function convert () {
+//   var c = document.querySelector("#c").value;
+// with inputs, we want to get the value (not innerHTML)}
+
+// STEP 2 = define var and then test the var are working through console log
+// function convert () {
+//   var c = document.querySelector("#c").value;
+//   var f = c * 9/5 +32;
+//   console.log(f);
+// }
+
+// include parse function in case the user types in "4"
+// function convert () {
+//   var c = document.querySelector("#c").value;
+//   var f = parseFloat(c) * 9/5 + 32;
+//   console.log(f);
+// }
+
+//STEP 3 - go and get the data we want to modify which is in the h3 and we are giving it a whole new value (with the '=' we are reassigning the information)
+// function convert () {
+//   var c = document.querySelector("#c").value;
+//   var f = parseFloat(c) * 9/5 + 32;
+//
+//   document.querySelector("h3").innerHTML = "Result:  " + f + "&degF";
+// }
+
+function convert () {
+  var c = document.querySelector("#c").value;
+  var f = parseFloat(c) * 9/5 + 32;
+
+  document.querySelector("h3").innerHTML = "Result: " + f + "°F";
 }
 
-document.querySelector("button").onclick = conversion;
+// Put this at the end of the function because we want to do on click of the button, not as we load the page
+
+document.querySelector("#submit").onclick = convert;
+
+
+//MY ATTEMPT AT HOMEWORK
+ // function conversion () {
+//   var c = document.querySelector(".input").value;
+//   var f = c*9/5+32;
+//
+//   document.querySelector("span").innerHTML = f;
+// }
+//
+// document.querySelector("button").onclick = conversion;
--- FEWD/Week04/lesson08/homework/temp_converter/js/index2a.js
+++ FEWD/Week04/lesson08/homework/temp_converter/js/index2a.js
@@ -1,15 +1,75 @@
-//Do stuff
+// // GRAB THE DATA FIRST
+// function convert () {
+//   var c = document.querySelector("#c").value;
+//   var f = document.querySelector("#f").value;
 
-function conversionToFahrenheit () {
-  var c = document.querySelector(".a").value;
-  var f = c*5/9+32;
-  var d = (c-32)*(5/9);
+// Then there are two different things we want to do, so use if / else function (do not need else if, as it is one or the other)
+// function convert () {
+//   var c = document.querySelector("#c").value;
+//   var f = document.querySelector("#f").value;
+//
+//   if () {
+//
+//   } else {
+//
+//   }
 
-  if (document.querySelector(".b").value === "Fahrenheit") {
-    document.querySelector("span").innerHTML = f;
+// WRITE OUT PSUEDO CODE
+// function convert () {
+//   var c = document.querySelector("#c").value;
+//   var f = document.querySelector("#f").value;
+//
+//   if (the_user_filled_out_celcius) {
+//     //convert celcius to fahrenheit
+//   } else {
+//     // convert to fahrenheit to celcius
+//   }
+
+// //   document.querySelector("h3").innerHTML = "Result: " + conversion + "°" + scale; changed the f to 'conversion' as could be f or c and then we add scale. The 'conversion' and 'scale' is where we need data, therefore they are variables and we need to declare var
+// var c = document.querySelector("#c").value;
+// var f = document.querySelector("#f").value;
+// var conversion, scale; (we do not yet know how these are going to be defined)
+
+// if (c does not equal empty string in js this is c !== "". We are checking that there is something in this box) {
+//   //convert celcius to fahrenheit
+// } else {
+//   // convert to fahrenheit to celcius
+// }
+
+// So we know the user has filled in celcius, so know we define the conversion which is the variable conversion
+// i.e. c = f * 9/5 + 32 and then we can define scale as well as 'F' in this case within the {} so the variables are then specific to those curly brackets
+
+function convert () {
+  var c = document.querySelector("#c").value;
+  var f = document.querySelector("#f").value;
+  var conversion, scale;
+
+  if (c !== "") {
+    conversion = c * 9/5 + 32;
+    scale = "F";
   } else {
-    document.querySelector("span").innerHTML = d;
+    conversion = (f - 32) * 5/9;
+    scale = "C";
   }
+
+  document.querySelector("h3").innerHTML = "Result: " + conversion + "°" + scale;
 }
 
-document.querySelector("button").onclick = conversionToFahrenheit;
+document.querySelector("#submit").onclick = convert;
+
+
+
+//MY ATTEMPT AT HOMEWORK
+// function conversionToFahrenheit () {
+//   var c = document.querySelector(".a").value;
+//   var f = c*5/9+32;
+//   var d = (c-32)*(5/9);
+//
+//   if (document.querySelector(".b").value === "Fahrenheit") {
+//     document.querySelector("span").innerHTML = f;
+//   } else {
+//     document.querySelector("span").innerHTML = d;
+//   }
+// }
+//
+// document.querySelector("button").onclick = conversionToFahrenheit;
