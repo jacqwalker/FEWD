fc49f2de632d9d5f022f7ee05116188478d9a492
jacqwalker <blackjack.walker@gmail.com>
2016-08-21 20:29:37 +0100

    City changes background images

--- FEWD/Week05/lesson10/classwork/city_changer/js/index.js
+++ FEWD/Week05/lesson10/classwork/city_changer/js/index.js
@@ -1,16 +1,26 @@
-  // do something
-  // define var
-  // run function
-function cityChanger (city) {
-  city.preventDefault();
-  console.log("say hello");
-
-  var city = document.querySelector("#city-type").value;
-  // $("#city-type").val();
-
-  // if (city === "London" ) {
-  //   $("body").css("backgound-image", "url(./images/london.jpg)");
-  //  }
+function switchCity (city) {
+  document.querySelector("body").className = city;
 }
 
+function cityChanger (event) {
+  event.preventDefault();
+
+  var city = $("#city-type").val();
+
+  if (city == "New York City" || city == "New York City".toLowerCase() || city == "nyc") {
+    switchCity("nyc");
+  } else if (city == "San Francisco" || city == "sf") {
+    switchCity("sf");
+  } else if (city == "Los Angeles") {
+    switchCity("la");
+  } else if (city == "Austin") {
+    switchCity("austin");
+  } else if (city = "Sydney") {
+    switchCity("sydney");
+  } else {
+    switchCity("london");
+  }
+}
+
+// document.querySelector("#submit-btn").onclick = cityChanger;
 $("#submit-btn").click(cityChanger);
