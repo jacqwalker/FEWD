b21a472e7c5ec616ddcda0281dff263a3921b9db
nicollecastrog <nicollecastrog@gmail.com>
2016-09-06 00:02:36 +0100

    week7 squashed commit

---
+++ FEWD/Week07/lesson13/classwork/responsive_articles/js/main.js
@@ -0,0 +1,26 @@
+if ($(window).width() >= 650) {
+  $("ul").show();
+  $("button").hide();
+} else {
+  $("ul").hide();
+  $("button").show();
+}
+
+function resize () {
+  var screenWidth = $(window).width();
+
+  if (screenWidth >= 650) {
+  $("ul").show();
+  $("button").hide();
+} else {
+  $("ul").hide();
+  $("button").show();
+}
+}
+
+function hamburgerMenu () {
+  $("ul").toggle();
+}
+
+$(".hamburger").click(hamburgerMenu);
+$(window).resize(resize);
