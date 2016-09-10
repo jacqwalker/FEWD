2f31d8b16586354a70fb522050b395023e51acf9
jacqwalker <blackjack.walker@gmail.com>
2016-09-10 17:58:05 +0100

    Responsive articles commit

--- FEWD/Week07/lesson13/classwork/responsive_articles/js/main.js
+++ FEWD/Week07/lesson13/classwork/responsive_articles/js/main.js
@@ -1,26 +1,26 @@
-if ($(window).width() >= 650) {
-  $("ul").show();
-  $("button").hide();
-} else {
-  $("ul").hide();
-  $("button").show();
-}
-
-function resize () {
-  var screenWidth = $(window).width();
-
-  if (screenWidth >= 650) {
-  $("ul").show();
-  $("button").hide();
-} else {
-  $("ul").hide();
-  $("button").show();
-}
-}
-
+// if ($(window).width() >= 650) {
+//   $("ul").show();
+//   $("button").hide();
+// } else {
+//   $("ul").hide();
+//   $("button").show();
+// }
+//
+// function resize () {
+//   var screenWidth = $(window).width();
+//
+//   if (screenWidth >= 650) {
+//   $("ul").show();
+//   $("button").hide();
+// } else {
+//   $("ul").hide();
+//   $("button").show();
+// }
+// }
+//
 function hamburgerMenu () {
-  $("ul").toggle();
+  $("ul").toggleClass("visible");
 }
 
 $(".hamburger").click(hamburgerMenu);
-$(window).resize(resize);
+// $(window).resize(resize);
