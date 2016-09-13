83c3d5f8042de9d031823e1003eb846b53cb831c
jacqwalker <blackjack.walker@gmail.com>
2016-09-13 19:11:33 +0100

    Shop page initial commit

--- FEWD/Walkr Kidz Wear/js/main.js
+++ FEWD/Walkr Kidz Wear/js/main.js
@@ -2,4 +2,14 @@ function showNavBar () {
   $("ul").toggleClass("visible");
 };
 
+// function changeSlider () {
+//   $(".section1").css("background-image", "url(./images/desktop/slider2.jpg");
+// };
+//
+// function restoreImage () {
+//   $(".section1").css("background-image", "url(./images/desktop/slider.jpg");
+// }
+
 $(".hamburger").click(showNavBar);
+// $(".click2").click(changeSlider);
+// $(".click1").click(restoreImage);
