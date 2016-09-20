5db21751475f11bf50ad2b475494c3a76cd85c98
jacqwalker <blackjack.walker@gmail.com>
2016-09-20 11:05:06 +0100

    Contact form styling

--- FEWD/Walkr Kidz Wear/js/main.js
+++ FEWD/Walkr Kidz Wear/js/main.js
@@ -2,14 +2,16 @@ function showNavBar () {
   $("ul").toggleClass("visible");
 };
 
-// function changeSlider () {
-//   $(".section1").css("background-image", "url(./images/desktop/slider2.jpg");
-// };
-//
-// function restoreImage () {
-//   $(".section1").css("background-image", "url(./images/desktop/slider.jpg");
-// }
+function changeSlider () {
+  console.log("say hello");
+  $(".section1").css("background-image", "url(./images/desktop/slider2.jpg");
+};
+
+function restoreImage () {
+  console.log("say hello");
+  $(".section1").css("background-image", "url(./images/desktop/slider.jpg");
+}
 
 $(".hamburger").click(showNavBar);
-// $(".click2").click(changeSlider);
-// $(".click1").click(restoreImage);
+$(".click2").click(changeSlider);
+$(".click1").click(restoreImage);
